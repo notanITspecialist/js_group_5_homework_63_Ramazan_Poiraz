4970c895fb935be37023f2bdbe97bfe711035bb7
Ramazan Poiraz <assasins11122001@gmail.com>
2019-12-18 13:40:03 +0300

    Создал компонент Contacts

---
+++ src/components/contacts/contacts.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Contacts = () => {
+    return (
+        <div>
+            <p>Ссылки на сайты наших пользователей</p>
+            <ul>
+                <li><a href="https://www.google.com/">GOOGLE</a></li>
+                <li><a href="https://twitter.com/">TWITTER</a></li>
+                <li><a href="https://www.facebook.com/">FACEBOOK</a></li>
+            </ul>
+        </div>
+    );
+};
+
+export default Contacts;
