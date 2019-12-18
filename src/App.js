6a10dc88d836813a47b0332abd0baa6958d16fe8
Ramazan Poiraz <assasins11122001@gmail.com>
2019-12-18 10:51:32 +0300

    Добавил компонент со списком всех постов, и добавление этого списка в state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import AddPost from "./components/addPost/addPost";
+import PostList from "./components/postList/postList";
 
 class App extends Component {
     render() {
         return (
             <div>
                 <AddPost />
+                <PostList />
             </div>
         );
     }
