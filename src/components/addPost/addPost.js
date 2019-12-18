e695080f2a664252d153b93e42f9462c4535cd02
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-12-18 11:48:58 +0200

    Добавил editor на страницу и стилизовал Add

--- src/components/addPost/addPost.js
+++ src/components/addPost/addPost.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import axios from 'axios'
 import { Button, Form, Input} from 'reactstrap';
+import { Editor } from 'react-draft-wysiwyg';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import './addPost.css';
 
 class AddPost extends Component {
     state = {
@@ -26,11 +29,19 @@ class AddPost extends Component {
 
     render() {
         return (
-            <div style={{width: '20%'}}>
+            <div>
+                <h1 className='my-4'>Add new post</h1>
                 <Form onSubmit={this.addPost}>
-                    <Input onChange={this.changeTitle} value={this.state.title} placeholder="Title" />
-                    <Input onChange={this.changeText} value={this.state.text} placeholder="Text" />
-                    <Button>SAVE</Button>
+                    <label htmlFor="title" className='mb-4'>
+                        <p className='m-0'>Title</p>
+                        <Input onChange={this.changeTitle} value={this.state.title} id='title' />
+                    </label>
+                    <p className='m-0'>Description</p>
+                    <Editor
+                        EditorState={this.changeText}
+                        editorClassName='editor'
+                    />
+                    <Button className='my-40'>Add</Button>
                 </Form>
             </div>
         );
