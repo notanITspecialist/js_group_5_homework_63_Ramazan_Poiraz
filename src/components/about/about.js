4b93ef6563e00e3ff418f4bbf0b6df1f72743401
Ramazan Poiraz <assasins11122001@gmail.com>
2019-12-18 13:32:41 +0300

    Сделал компонент about

---
+++ src/components/about/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>Информация о нас</h2>
+            <p>Какая нибудь информация о нас</p>
+        </div>
+    );
+};
+
+export default About;
