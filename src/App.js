310db3f758dd13ac297546ae2cb00294577fb05e
Ramazan Poiraz <assasins11122001@gmail.com>
2019-12-18 09:29:21 +0300

    Удалил ненужные файлы, установил reactstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class App extends Component {
+  add =() => {
+    const date = new Date();
+    const objDate = date.getMonth() + '.' + date.getDate() + '.' + date.getFullYear();
+    const info = {title: 'Title', text: 'Text', date:objDate};
+    axios.post('https://lesson-64-49739.firebaseio.com/blog.json', info)
+  };
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
