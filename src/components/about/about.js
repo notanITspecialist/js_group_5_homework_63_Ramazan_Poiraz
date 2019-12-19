d8f74b159d4773f4355ce43c5d1558b0ace62da5
Ramazan Poiraz <assasins11122001@gmail.com>
2019-12-19 08:55:04 +0300

    Сделал добовление информации о странице about в state

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import axios from 'axios'
 import Button from "reactstrap/lib/Button";
 
-const About = () => {
-    return (
-        <div style={{marginTop: '30px'}}>
-            <h2>Информация о нас</h2>
-            <p>Какая нибудь информация о нас</p>
-            <Button>Edit</Button>
-        </div>
-    );
-};
+import React, {Component} from 'react';
+
+class About extends Component {
+
+    state = {
+        data: {}
+    };
+
+    async componentDidMount() {
+        let data = await axios.get('https://lesson-64-49739.firebaseio.com/about.json');
+        data = data.data[Object.keys(data.data)[0]];
+        this.setState({data});
+    }
+
+    render() {
+        return (
+            <div style={{marginTop: '30px'}}>
+                <h2>Информация о нас</h2>
+                <p>Какая нибудь информация о нас</p>
+                <Button>Edit</Button>
+            </div>
+        );
+    }
+}
 
 export default About;
